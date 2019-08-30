ff1ec2f6865792de481a747a40d5eafee5fd8826
dange <dange0.tw@gmail.com>
2019-08-30 23:19:45 +0800

    Add Windows Defender as Victim

--- test_agent_chainer.py
+++ test_agent_chainer.py
@@ -11,25 +11,35 @@ ACTION_LOOKUP = {i: act for i, act in enumerate(manipulate.ACTION_TABLE.keys())}
 
 from train_agent_chainer import create_acer_agent
 import gym
+import os
+import hashlib
+import shutil
 
 def evaluate( action_function ):
     success=[]
     misclassified = []
     for sha256 in sha256_holdout:
+        print("#########{}#########".format(sha256))
         success_dict = defaultdict(list)
-        bytez = interface.fetch_file(sha256)
-        label = interface.get_label_local(bytez)
+        bytez_o = interface.fetch_file(sha256)
+        label = interface.get_label_windefender(bytez_o)
         if label == 0.0:
             misclassified.append(sha256)
             continue # already misclassified, move along
+        bytez = bytez_o
         for _ in range(MAXTURNS):
             action = action_function( bytez )
             print(action)
             success_dict[sha256].append(action)
             bytez = manipulate.modify_without_breaking( bytez, [action] )
-            new_label = interface.get_label_local( bytez )
+            new_label = interface.get_label_windefender( bytez )
             if new_label == 0.0:
-                success.append(success_dict)
+                success.append(success_dict[sha256])
+                os.mkdir(PATH_SAVE + '/' + sha256)
+                with open(PATH_SAVE + '/' + sha256 + '/orign', 'wb') as f:
+                    f.write(bytez_o)
+                with open(PATH_SAVE + '/' + sha256 + '/modified' ,'wb') as f:
+                    f.write(bytez)
                 break
     return success, misclassified # evasion accuracy is len(success) / len(sha256_holdout)
 
@@ -46,13 +56,18 @@ def get_latest_model_from(basedir):
 
     assert lastmodel >= 0, "No saved models!"
     return os.path.join(basedir, str(lastmodel))
+def create_output_folder(path):
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    os.mkdir(path)
 
 if __name__ == '__main__':
     # baseline: choose actions at random
+    PATH_SAVE = 'evaded/random_output'
+    create_output_folder(PATH_SAVE)
     random_action = lambda bytez: np.random.choice( list(manipulate.ACTION_TABLE.keys()) )
     random_success, misclassified = evaluate( random_action )
     total = len(sha256_holdout) - len(misclassified) # don't count misclassified towards success
-
     ENV_NAME = 'malware-test-v0' 
     env = gym.make(ENV_NAME)
 
@@ -67,19 +82,12 @@ if __name__ == '__main__':
 
     agent = create_acer_agent(env)
     # pull latest stored model
-    last_model_dir = get_latest_model_from('models/acer_chainer')
+    PATH_SAVE = 'evaded/blackbox_output'
+    create_output_folder(PATH_SAVE)
+    last_model_dir = get_latest_model_from('models/acer_chainer_MD')
     agent.load( last_model_dir )
-    success, _ = evaluate( agent_policy(agent) )
-
-    agent_score = create_acer_agent(env)
-    # pull latest stored model
-    last_model_dir = get_latest_model_from('models/acer_score_chainer')
-    agent_score.load( last_model_dir )
-
-    score_success, _ = evaluate( agent_policy(agent) )
+    success, misclassified_blackbox = evaluate( agent_policy(agent) )
 
     print("Success rate of random chance: {}\n".format( len(random_success) / total ))
     print("Success rate (black box): {}\n".format( len(success) / total ) )
-    print("Success rate (score): {}\n".format( len(score_success) / total ) )          
-
 
